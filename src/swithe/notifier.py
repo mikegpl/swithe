cdcc4815f02a7b4fbe6845c6ded076c3097e4252
Michał Grabowski <mkg.grabowski@gmail.com>
2022-04-07 18:37:32 +0200

    Fix inconsistent indentation issue

--- src/swithe/notifier.py
+++ src/swithe/notifier.py
@@ -2,11 +2,11 @@ from contextlib import ContextDecorator
 
 
 class notify_on_finish(ContextDecorator):
-    def __enter__(self):
-    	print('Starting')
-        return self
+	def __enter__(self):
+		print('Starting')
+		return self
 
-    def __exit__(self, *exc):
-        print("And I'm notifying right now")
-        print(f"This exception might not exist: {exc}")
-        return False
+	def __exit__(self, *exc):
+		print("And I'm notifying right now")
+		print(f"This exception might not exist: {exc}")
+		return False
